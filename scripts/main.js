4382d96d40eaea4b6d4ea8a3b07cd4eef65aa037
Garry <Garry.huang@stonybrook.edu>
2016-09-25 04:56:09 -0400

    Added more JS functions and profile picture

--- scripts/main.js
+++ scripts/main.js
@@ -2,13 +2,18 @@ document.getElementById("yes").onclick = yes;
 document.getElementById("no").onclick = no;
 document.getElementById("perhaps").onclick = perhaps;
 var element = document.getElementById("ChangeText");
+var category = document.getElementById("category");
+category.innerHTML = "HI";
 element.innerHTML = "Cincinatti Zoo";
 function yes(){
+	element.innerHTML = "fuck you"
 	alert("user pressed yes");
 }
 function no(){
+	element.innerHTML = "same"
 	alert("user pressed no");
 }
 function perhaps(){
+	element.innerHTML = "???"
 	alert("user pressed perhaps");
 }
