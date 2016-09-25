56399854593677c6e94172e8920adc062cd520e4
cli473 <calvin.li@stonybrook.edu>
2016-09-25 08:16:29 -0400

    Update main.js
    added in some comments to help fill in functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,27 @@
 document.getElementById("yes").onclick = yes;
 document.getElementById("no").onclick = no;
-document.getElementById("perhaps").onclick = perhaps;
+document.getElementById("unsure").onclick = unsure;
 var element = document.getElementById("ChangeText");
 var category = document.getElementById("category");
-category.innerHTML = "HI";
-element.innerHTML = "Cincinatti Zoo";
+category.innerHTML = "Harassment";
+<! (don't need to change this) element.innerHTML = "Cincinatti Zoo"; 
 function yes(){
-	element.innerHTML = "fuck you"
+	element.innerHTML = "Thanks for submitting your response, the poster will be dealt with accordingly."
 	alert("user pressed yes");
+    <! need to increment score by 1
+    <! need to increment timesChecked
+    <! need to check if timesChecked = 5 
 }
 function no(){
-	element.innerHTML = "same"
+	element.innerHTML = "Thanks for submitting your response, the poster will be dealt with accordingly."
 	alert("user pressed no");
+    <! need to increment timesChecked    
+    <! need to check if timesChecked = 5 
 }
-function perhaps(){
-	element.innerHTML = "???"
+function unsure(){
+	element.innerHTML = "You aren't very helpful but thanks for trying :)"
 	alert("user pressed perhaps");
+    <! need to increment score by 0.5
+    <! need to increment timesChecked
+    <! need to check if timesChecked = 5 
 }
