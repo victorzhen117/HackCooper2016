d7f2804504d69cc632ea9fe51e550e5dbb86606b
Garry <Garry.huang@stonybrook.edu>
2016-09-24 20:04:16 -0400

    getTweet method - includes tweet and date tweeted

--- src/twitter/api/TwitterAPI.java
+++ src/twitter/api/TwitterAPI.java
@@ -18,29 +18,30 @@ public class TwitterAPI {
     /**
      * @param args the command line arguments
      */
-public static void main(String[] args) {
-    ConfigurationBuilder cb = new ConfigurationBuilder();
-    cb.setDebugEnabled(true)
-    .setOAuthConsumerKey("53nUEtaL8hsyBS7HyCLZsdj5O")
-    .setOAuthConsumerSecret("5ZJ8TeXWj7zHOhK1OtvqxmjD0dIrHUjmtZ7rLOYc9cK0ovDcTF")
-    .setOAuthAccessToken("576227678-tDF0uPGF3bjCAFgB7CVRcAXCLWSm7pc7dgEzLVea")
-    .setOAuthAccessTokenSecret("LKHdmnEhlTPXkgZrI0jYejrOkhtcJTvf7Gz3kIBCeoFu7");
-        
-    TwitterFactory tf = new TwitterFactory(cb.build());
-    Twitter twitter = tf.getInstance();
-        System.out.println("1");
-        try {
-            Query query = new Query("hehe");
+    public static void main(String[] args) {
+        System.out.println(getTweet());
+    }
+    public static String getTweet(){
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true)
+        .setOAuthConsumerKey("53nUEtaL8hsyBS7HyCLZsdj5O")
+        .setOAuthConsumerSecret("5ZJ8TeXWj7zHOhK1OtvqxmjD0dIrHUjmtZ7rLOYc9cK0ovDcTF")
+        .setOAuthAccessToken("576227678-tDF0uPGF3bjCAFgB7CVRcAXCLWSm7pc7dgEzLVea")
+        .setOAuthAccessTokenSecret("LKHdmnEhlTPXkgZrI0jYejrOkhtcJTvf7Gz3kIBCeoFu7");
+
+        TwitterFactory tf = new TwitterFactory(cb.build());
+        Twitter twitter = tf.getInstance();   
+        String tweet = "";
+        try{
+            Query query = new Query("slut");
             QueryResult result = twitter.search(query);
             for (Status status : result.getTweets()) {
-                System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
+                tweet = status.getText() + " " + status.getCreatedAt().toString();
+            }
         }
-            System.exit(0);
-        } catch (TwitterException te) {
+        catch (TwitterException te){
             te.printStackTrace();
-            System.out.println("Failed to search tweets: " + te.getMessage());
-            System.exit(-1);
         }
+        return tweet;
     }
-    
 }
